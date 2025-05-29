de1891c63b9c06f7d610ff569d2d50d557bca195
Scacchetti <luiscacchetti07@gmail.com>
2025-05-29 19:18:33 -0300

    Funcoes no terminal e começando com Tkinter

--- src/AlertaBR/Pages/home.py
+++ src/AlertaBR/Pages/home.py
@@ -1,42 +1,19 @@
-# from tkinter import *
-# from tkinter import ttk
+import customtkinter as ctk
 
-# class FeetToMeters:
-    
-#     def __init__(self, root):
-#         root.title("Feet to Meters")
-#         mainframe = ttk.Frame(root, padding="3 3 12 12")
-#         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-#         root.columnconfigure(0, weight=1)
-#         root.rowconfigure(0, weight=1)
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue") 
 
-#         self.feet = StringVar()
-#         feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
-#         feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-#         self.meters = StringVar()
-#         ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
-#         ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
-
-#         ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-#         ttk.Label(mainframe, text="is equivalent to ").grid(column=1, row=2, sticky=E)
-#         ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-#         for child in mainframe.winfo_children():
-#             child.grid_configure(padx=5, pady=5)
-
-#         feet_entry.focus()
-#         root.bind("<Return>", self.calculate)
-        
-#     def calculate(self, *args):
-#         try:
-#             value = float(self.feet.get())
-#             self.meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-#         except ValueError:
-#             pass
+root = ctk.CTk()
+root.title("AlertaBR")
+root.iconbitmap("./images/favicon.ico")
+root.geometry("390x844")
 
 
+frame = ctk.CTkFrame(master=root, width=390, height=844)
+frame.pack(fill="both", expand=True)
 
-# root = Tk()
-# FeetToMeters(root)
-# root.mainloop()
+searchBar = ctk.CTkEntry(frame, width=350, height=55, corner_radius=30, placeholder_text="Pesquise Aqui", placeholder_text_color="gray")
+searchBar.pack(pady=40)
+searchBar.focus()
+
+root.mainloop()
