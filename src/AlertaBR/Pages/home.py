d8b64dde1486508fffb84c0efdbd2520ba221954
Scacchetti <luiscacchetti07@gmail.com>
2025-05-29 23:47:18 -0300

    Classes em Tkinter

--- src/AlertaBR/Pages/home.py
+++ src/AlertaBR/Pages/home.py
@@ -3,17 +3,27 @@ import customtkinter as ctk
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("blue") 
 
-root = ctk.CTk()
-root.title("AlertaBR")
-root.iconbitmap("./images/favicon.ico")
-root.geometry("390x844")
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title("AlertaBR")
+        self.geometry("390x844")
+        self.resizable(0, 0)
+        self.wm_iconbitmap(default="./images/favicon.ico")
+        self.eval("tk::PlaceWindow . center")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure((0, 1), weight=1)
+        
+            
+        frame = ctk.CTkFrame(master=self, width=390, height=844)
+        frame.pack(fill="both", expand=True)
 
 
-frame = ctk.CTkFrame(master=root, width=390, height=844)
-frame.pack(fill="both", expand=True)
+        searchBar = ctk.CTkEntry(frame, width=350, height=50, corner_radius=30, placeholder_text="Pesquise Aqui", placeholder_text_color="gray")
+        searchBar.pack(pady=80)
+        searchBar.focus()
 
-searchBar = ctk.CTkEntry(frame, width=350, height=55, corner_radius=30, placeholder_text="Pesquise Aqui", placeholder_text_color="gray")
-searchBar.pack(pady=40)
-searchBar.focus()
 
-root.mainloop()
+app = App()
+app.mainloop()
