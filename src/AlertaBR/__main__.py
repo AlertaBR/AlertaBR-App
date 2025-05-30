75c43d8d9b9c60d1d1fb0522145d8008eef343b2
Scacchetti <luiscacchetti07@gmail.com>
2025-05-29 22:57:12 -0300

    Adicionando Switch para troca de tela

--- src/AlertaBR/__main__.py
+++ src/AlertaBR/__main__.py
@@ -1,9 +1,10 @@
 from utils.tools import Color, menu, getLocalTime
+from utils.options import switchMenu
 
 def main():
     # Substituir pela localização atual no google maps
     print(f'Hora Atual: {getLocalTime()}')
-    resp = menu(['Buscar região de risco', 'Ligar para autoridades', 'Faça um report', 'Veja os reports da região' 'Configurações'], Color.YELLOW)
+    resp = menu(['Buscar região de risco', 'Ligar para autoridades', 'Faça um report', 'Veja os reports da região' 'Configurações', 'Login'], Color.YELLOW)
     switchMenu(resp)
 
 if __name__ == '__main__':
